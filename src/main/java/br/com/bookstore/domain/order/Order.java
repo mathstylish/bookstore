a2e96633c7f32fc55dea13c968544c437f053b78
Matheus Henrique <matheus.hpp135@gmail.com>
2023-11-30 14:31:54 -0300

    Application under fixing

--- src/main/java/br/com/bookstore/domain/order/Order.java
+++ src/main/java/br/com/bookstore/domain/order/Order.java
@@ -3,6 +3,7 @@ package br.com.bookstore.domain.order;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -12,7 +13,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -21,11 +22,12 @@ import java.util.Date;
 @Table(name = "orders")
 public class Order {
     @Id
+    @GeneratedValue
     private Integer id;
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
-    private Date date;
+    private Instant date;
 
     @ManyToOne
     @JoinColumn(name = "discount_id")
