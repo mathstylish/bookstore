417acd9c6047c7b7e7c8ccdb794adc89b039868b
Matheus <matheus.hpp135@gmail.com>
2023-11-30 13:35:13 -0300

    refactor(domain): add missing @GeneratedValue annotation

--- src/main/java/br/com/bookstore/domain/order/Order.java
+++ src/main/java/br/com/bookstore/domain/order/Order.java
@@ -3,6 +3,7 @@ package br.com.bookstore.domain.order;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,6 +22,7 @@ import java.util.Date;
 @Table(name = "orders")
 public class Order {
     @Id
+    @GeneratedValue
     private Integer id;
     @ManyToOne
     @JoinColumn(name = "customer_id")
